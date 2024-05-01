b5e1e2b9806b01b079ccfc60bf3f1252ea56f3a1
Sasha Devol <sasha.devol@protonmail.com>
2024-04-30 23:26:51 -0500

    Add nix compatibility / convenience setup

--- flake.nix
+++ flake.nix
@@ -1,39 +1,24 @@
 {
   description = "Lockdown python dependency reproducibility, attempt 56";
 
-  inputs.pyproject-nix.url = "github:nix-community/pyproject.nix";
-  inputs.pyproject-nix.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.pyproject.url = "github:nix-community/pyproject.nix";
+  inputs.pyproject.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.systems.url = "github:nix-systems/default";
 
   outputs = {
     nixpkgs,
-    pyproject-nix,
+    pyproject,
+    systems,
     ...
   }: let
-    # Loads pyproject.toml into a high-level project representation
-    # Do you notice how this is not tied to any `system` attribute or package sets?
-    # That is because `project` refers to a pure data representation.
-    project = pyproject-nix.lib.project.loadPyproject {
+    project = pyproject.lib.project.loadPyproject {
       # Read & unmarshal pyproject.toml relative to this project root.
       # projectRoot is also used to set `src` for renderers such as buildPythonPackage.
       projectRoot = ./.;
     };
-
-    # This example is only using aarch64-darwin
-    pkgs = nixpkgs.legacyPackages.aarch64-darwin;
-
-    # We are using the default nixpkgs Python3 interpreter & package set.
-    #
-    # This means that you are p word2numberurposefully ignoring:
-    # - Version bounds
-    # - Dependency sources (meaning local path dependencies won't resolve to the local path)
-    #
-    # To use packages from local sources see "Overriding Python packages" in the nixpkgs manual:
-    # https://nixos.org/manual/nixpkgs/stable/#reference
-    #
-    # Or use an overlay generator such as pdm2nix:
-    # https://github.com/adisbladis/pdm2nix
-    word2number =
-      pkgs.python311Packages.buildPythonPackage
+    eachSystem = nixpkgs.lib.genAttrs (import systems);
+    buildWordNumber = ps:
+      ps.python311Packages.buildPythonPackage
       rec {
         pname = "word2number";
         version = "1.03";
@@ -44,43 +29,53 @@
           rev = "af1448e4aa86a177968a1c5537731d1e478cce29";
           ref = "v1.03";
         };
-        nativeBuildInputs = [pkgs.python311Packages.setuptools];
+        nativeBuildInputs = [ps.python311Packages.setuptools];
         postUnpack = ''
           if [ ! -f $sourceRoot/setup.py ]; then
             echo "from setuptools import setup; setup(name='${pname}')" > $sourceRoot/setup.py
           fi
         '';
       };
-    python = pkgs.python311.override (oldAttrs: {
-      packageOverrides = self: super: {
-        inherit word2number;
-        tqdm-multiprocess = pkgs.python311Packages.tqdm;
-      };
-    });
+      buildPython = {
+        ps,
+        word2number,
+      }:
+      ps.python311.override (oldAttrs: {
+        packageOverrides = self: super: {
+          inherit word2number;
+          tqdm-multiprocess = ps.python311Packages.tqdm;
+        };
+      });
   in {
-    # Create a development shell containing dependencies from `pyproject.toml`
-    devShells.aarch64-darwin.default = let
-      attr = project.renderers.withPackages {
-        inherit python;
+    packages = eachSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      word2number = buildWordNumber pkgs;
+      python = buildPython {
+        inherit word2number;
+        ps = pkgs;
       };
-
-      # Returns a wrapped environment (virtualenv like) with all our packages
-      pythonEnv = python.withPackages attr;
-    in
-      # Create a devShell like normal.
-      pkgs.mkShell {
-        packages = [pythonEnv];
-      };
-
-    # Build our package using `buildPythonPackage
-    packages.aarch64-darwin.default = let
-      attrs = project.renderers.buildPythonPackage {
-        inherit python;
+      packageAttrs = pyproject.lib.renderers.buildPythonPackage {
+        inherit python project;
       };
     in
-      python.pkgs.buildPythonPackage (attrs
+      python.pkgs.buildPythonPackage (packageAttrs
         // {
           #          env.CUSTOM_ENVVAR = "hello";
-        });
+        }));
+
+    devShells = eachSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+        word2number = buildWordNumber pkgs;
+        python = buildPython {
+          ps = pkgs;
+          inherit word2number;
+        };
+      in {
+        default = pkgs.mkShell {
+          packages = [python];
+        };
+      }
+    );
   };
 }
