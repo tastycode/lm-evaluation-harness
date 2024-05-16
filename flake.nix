8bdb15075d7502c30863aac4e52d6651e5962681
Sasha Devol <sasha@nola-ai.com>
2024-05-16 02:15:56 +0000

    Rebuild flake based on yet another cuda configuration

--- flake.nix
+++ flake.nix
@@ -1,37 +1,31 @@
 {
   description = "Lockdown python dependency reproducibility, attempt 56";
-  inputs.nixpkgs.url = "github:numtide/nixpkgs-unfree";
-  inputs.nixpkgs.inputs.nixpkgs.follows = "nixpkgs-unstable";
 
-  inputs.nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:numtide/nixpkgs-unfree";
+   # nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    pyproject.url = "github:nix-community/pyproject.nix";
+    pyproject.inputs.nixpkgs.follows = "nixpkgs";
+    systems.url = "github:nix-systems/default";
+  };
 
-  # Optionally, pull pre-built binaries from this project's cache
-  nixConfig.extra-substituters = [ "https://numtide.cachix.org" ];
-  nixConfig.extra-trusted-public-keys = [ "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE=" ];
+  nixConfig = {
+    extra-substituters = [ "https://numtide.cachix.org" "https://cuda-maintainers.cachix.org" ];
+    extra-trusted-public-keys = [ "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE=" "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="];
+  };
 
-  inputs.pyproject.url = "github:nix-community/pyproject.nix";
-  inputs.pyproject.inputs.nixpkgs.follows = "nixpkgs";
-  inputs.systems.url = "github:nix-systems/default";
-
-  outputs = {
-    nixpkgs,
-    pyproject,
-    systems,
-    ...
-  }: let
+  outputs = { self, nixpkgs, systems, pyproject, ... } @ inputs: let
+    forEachSystem = nixpkgs.lib.genAttrs (import systems);
     project = pyproject.lib.project.loadPDMPyproject {
-      # Read & unmarshal pyproject.toml relative to this project root.
-      # projectRoot is also used to set `src` for renderers such as buildPythonPackage.
       projectRoot = ./.;
     };
-    eachSystem = nixpkgs.lib.genAttrs (import systems);
+
     buildWordNumber = ps:
-      ps.python311Packages.buildPythonPackage
-      rec {
+      ps.python311Packages.buildPythonPackage rec {
         pname = "word2number";
         version = "1.03";
         format = "pyproject";
-        build-system = [];
         src = builtins.fetchGit {
           url = "https://github.com/bestmahdi2/word2number.git";
           rev = "af1448e4aa86a177968a1c5537731d1e478cce29";
@@ -43,47 +37,72 @@
             echo "from setuptools import setup; setup(name='${pname}')" > $sourceRoot/setup.py
           fi
         '';
-        propagatedBuildInputs = [
-           ps.python311Packages.tomli
-        ];
       };
-      buildPython = {
-        pkgs,
-        word2number,
-      }:
-      (pkgs.python311.override { 
-		packageOverrides = self: super: {
-                  inherit word2number;
-                };
-	}).withPackages (project.renderers.withPackages {
-               python = pkgs.python311;
-         });
-  in  rec {
-    packages = eachSystem (system: let     
+
+    buildPython = {
+      pkgs,
+      word2number,
+    }: (pkgs.python311.override {
+      packageOverrides = self: super: {
+        inherit word2number;
+        torch = super.torch-bin;
+        sphinxcontrib-jquery = super.sphinxcontrib-jquery.overridePythonAttrs (oldAttrs: {
+          propagatedBuildInputs = [ self.sphinx ];
+        });
+        accelerate = super.accelerate.overridePythonAttrs (oldAttrs: {
+          propagatedBuildInputs = [ self.huggingface-hub self.numpy self.psutil self.torch self.safetensors ];
+        });
+        buildPythonPackage = args: super.buildPythonPackage (args // { doCheck = false; });
+      };
+    }).withPackages (project.renderers.withPackages {
+      python = pkgs.python311;
+    });
+
+  in {
+    devShells = forEachSystem (system: let
       pkgs = nixpkgs.legacyPackages.${system};
       word2number = buildWordNumber pkgs;
       python = buildPython {
         inherit pkgs word2number;
-      }; in python);
-
-    devShells = eachSystem (
-      system: let
-        pkgs = nixpkgs.legacyPackages.${system};
-        word2number = buildWordNumber pkgs;
-        python = buildPython {
-          inherit pkgs word2number;
-        };
-      in {
-        default = pkgs.mkShell {
-                preCheck = ''
-			export TMPDIR=/tmp
-
-		'';
-	  dontUnpack = true;
-          doCheck = false;
-          packages = [python pkgs.pdm];
-        };
-      }
-    );
-	};
+      };
+    in {
+      default = pkgs.mkShell {
+        preCheck = ''
+          export TMPDIR=/tmp
+        '';
+        dontUnpack = true;
+        doCheck = false;
+        packages = [
+          python
+          pkgs.hyperfine
+          pkgs.pdm
+          pkgs.sphinx
+          pkgs.git
+          pkgs.zlib
+          pkgs.stdenv.cc.cc.lib
+        ];
+        shellHook = ''
+          export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [
+            pkgs.gcc-unwrapped.lib
+            pkgs.linuxPackages_latest.nvidia_x11
+            pkgs.zlib
+            pkgs.cmake
+            pkgs.cudaPackages.cudatoolkit
+            pkgs.cudaPackages.cudnn
+            pkgs.cudaPackages.libcublas
+            pkgs.cudaPackages.libcurand
+            pkgs.cudaPackages.libcufft
+            pkgs.cudaPackages.libcusparse
+            pkgs.cudaPackages.cuda_nvtx
+            pkgs.cudaPackages.cuda_cupti
+            pkgs.cudaPackages.cuda_nvrtc
+            pkgs.cudaPackages.nccl
+          ]}
+          export CUDA_HOME=${pkgs.cudaPackages.cudatoolkit}
+          export CUDA_PATH=${pkgs.cudaPackages.cudatoolkit}
+        '';
+      };
+    });
+  };
 }
+
